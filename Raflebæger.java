1b1d69eb43d9cdfb1bce542d523c99201b5cd289
losamigoss <32457293+losamigoss@users.noreply.github.com>
2017-11-08 10:13:36 +0100

    Update Raflebæger.java
    Opdatering med terninger med array

--- Raflebæger.java
+++ Raflebæger.java
@@ -1,17 +1,15 @@
 package Spil;
 import Spil.Terning;
 public class Raflebæger {
-	
-	Terning t1 = new Terning("terning1");
-	Terning t2 = new Terning("terning2");
+	Terning t1 = new Terning(new int[] {1,2,3,4,5,6});
+	Terning t2 = new Terning(new int[] {1,2,3,4,5,6});
 	String Raflebæger;
 	public int slag1, slag2;
 	public int samletVærdi;
 	
-	public Raflebæger (String rb){
-		Raflebæger=rb;
+	public Raflebæger (){
+		
 	}
-	
 	
 	public void slå(){
 		slag1 = t1.slag();
@@ -42,5 +40,7 @@ public class Raflebæger {
 	public int getEnkeltVærdi2() { //enkeltværdi for terning 2
 		return slag2;
 	}
+	
+	
 	
 }
