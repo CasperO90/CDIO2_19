f13f3b02bc918a45905a9b94ec191702bb42772d
losamigoss <32457293+losamigoss@users.noreply.github.com>
2017-11-10 08:30:36 +0100

    Update Konto.java
    Opdatering hvor man ikke kan hæve over og får besked herved

--- Konto.java
+++ Konto.java
@@ -13,15 +13,31 @@ public class Konto {
 	}
 	
 	public void setBeholdning(int nyBeholdning) {
-		beholdning=nyBeholdning;
+		if (nyBeholdning<0) {
+		beholdning=0;
+		}
+		else {
+			beholdning = nyBeholdning;
+		}
+		
 	}
 	
 	public void indsætPenge(int indsætning) {
+		if (indsætning>0) {
 		beholdning=beholdning+indsætning;
+		}
 	}
 	
-	public void hævPenge(int hævning) {
+	public String hævPenge(int hævning) {
+		
+		if (beholdning-hævning>=0) {
 		beholdning=beholdning-hævning;
+		return "Transaktion gennemført.";
+		}
+		
+		else {
+			return "Transaktion ikke gennemført. Du har ikke råd.";
+		}
 	}
 	
 	public String toString() {
